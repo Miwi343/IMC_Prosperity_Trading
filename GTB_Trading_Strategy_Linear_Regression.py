3378706066e9f45cde4768e2565e26f8d3f2fbd7
Mihir Kulshreshtha <75397268+Miwi343@users.noreply.github.com>
2024-03-18 21:44:49 -0400

    Template for adding linear regression later.

--- GTB_Trading_Strategy_Linear_Regression.py
+++ GTB_Trading_Strategy_Linear_Regression.py
@@ -13,13 +13,12 @@ class Trader:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
             #use linear regresion to adjust the acceptable price
-            
-            t = 1
-            p = []
-            p.append(t)
-            # jokes
+            #
+            #Insert Linear Regression code here
+            #
+            #    
                 
-        
+                
             acceptable_price = 12;  # Participant should calculate this value
             print("Acceptable price : " + str(acceptable_price))
             print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
